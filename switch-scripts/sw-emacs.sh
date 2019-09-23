2b510be14d5486505037faa67fdbea0c5fc04d9d
David Liu <xdavidliu@gmail.com>
2019-09-23 11:09:10 -0400

    Update sw-emacs.sh

--- switch-scripts/sw-emacs.sh
+++ switch-scripts/sw-emacs.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-wmctrl -x -a emacs.Emacs || emacs
+wmctrl -x -a emacs || emacs
 # https://unix.stackexchange.com/questions/303628/switch-among-windows-with-supernumbers-win1-2-3-etc/543026#543026
